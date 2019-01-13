9d8cad03f04f09f05eabe24839e52cddf96b5976
bohdanbirdie <voja4ok@yahoo.com>
2019-01-14 00:49:19 +0100

    Updates

---
+++ precache-manifest.393b274789a074e53920df99986bdddb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd6bb94c892250babd6",
+    "url": "/stl-obj-demo/static/js/runtime~main.1dd6bb94.js"
+  },
+  {
+    "revision": "9815da22ae37f0b1547c",
+    "url": "/stl-obj-demo/static/js/main.9815da22.chunk.js"
+  },
+  {
+    "revision": "a0241c9693f6e60533e4",
+    "url": "/stl-obj-demo/static/js/1.a0241c96.chunk.js"
+  },
+  {
+    "revision": "9815da22ae37f0b1547c",
+    "url": "/stl-obj-demo/static/css/main.8227627f.chunk.css"
+  },
+  {
+    "revision": "b0d00c44ece337e68dc7e006a164f45c",
+    "url": "/stl-obj-demo/index.html"
+  }
+];
